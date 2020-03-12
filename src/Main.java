4e21cc5c9546d28641ee1bd2e4e982e1a0f8bca8
lsmok1 <lsmok225@gmail.com>
2020-03-12 12:11:07 -0400

    Finished characterCreation method

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,9 @@ public class Main {
 //        return String.format();
 //    }
     public static void characterCreation() {
-        for (; ; ) {
+        boolean characterCreated = false;
+        //return "You're ready... Let's begin.";
+        do {
             Humans h1 = new Humans();
             System.out.println("Welcome to Humans vs. Goblins. Please enter your name: ");
             Scanner scan = new Scanner(System.in);
@@ -44,25 +46,101 @@ public class Main {
                         System.out.println("HP: " + h1.HP);
                         System.out.println("STR: " + h1.STR);
                         System.out.println("What attribute would you like to change? (1 - HP / 2 - STR)");
-                        for (; ; ) {
+                        do {
                             int hpOrStr = scan.nextInt();
-                            if (hpOrStr == 1) {
+                            while (hpOrStr == 1) {
                                 System.out.println("Points Left: " + h1.attPoints);
                                 System.out.println("HP: " + h1.HP);
                                 System.out.println("STR: " + h1.STR);
                                 System.out.println("How many points would you like to put into HP? (1-10)");
-                            } else if (hpOrStr == 2) {
+                                for (; ; ) {
+                                    int pointAllocate = scan.nextInt();
+                                    if (pointAllocate > h1.attPoints) {
+                                        System.out.println("You don't have that many points!");
+                                        break;
+                                    }
+                                    if (pointAllocate >= 10) {
+                                        System.out.println("Don't use all your points on one attribute!");
+                                        break;
+                                    } else {
+                                        h1.attPoints -= pointAllocate;
+                                        h1.HP += pointAllocate;
+                                        System.out.println("Points Left: " + h1.attPoints);
+                                        System.out.println("HP: " + h1.HP);
+                                        System.out.println("STR: " + h1.STR);
+                                        System.out.println("Are you fine with these changes? (Y/N)");
+                                        yOrN = scan.next().charAt(0);
+                                        if (yOrN == 'y' || yOrN == 'Y') {
+                                            hpOrStr = 0;
+                                            break;
+                                        } else if (yOrN == 'n' || yOrN == 'N') {
+                                            h1.attPoints += pointAllocate;
+                                            h1.HP -= pointAllocate;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (h1.attPoints == 0) {
+                                    System.out.println("Points Left: " + h1.attPoints);
+                                    System.out.println("HP: " + h1.HP);
+                                    System.out.println("STR: " + h1.STR);
+                                    characterCreated = true;
+                                    break;
+                                }
+                            }
+                            while (hpOrStr == 2) {
                                 System.out.println("Points Left: " + h1.attPoints);
                                 System.out.println("HP: " + h1.HP);
                                 System.out.println("STR: " + h1.STR);
                                 System.out.println("How many points would you like to put into STR? (1-10)");
+                                int pointAllocate = scan.nextInt();
+                                for (; ; ) {
+                                    if (pointAllocate > h1.attPoints) {
+                                        System.out.println("You don't have that many points!");
+                                        break;
+                                    }
+                                    if (pointAllocate >= 10) {
+                                        System.out.println("Don't use all your points on one attribute!");
+                                        break;
+                                    } else {
+                                        h1.attPoints -= pointAllocate;
+                                        h1.STR += pointAllocate;
+                                        System.out.println("Points Left: " + h1.attPoints);
+                                        System.out.println("HP: " + h1.HP);
+                                        System.out.println("STR: " + h1.STR);
+                                        System.out.println("Are you fine with these changes? (Y/N)");
+                                        yOrN = scan.next().charAt(0);
+                                        //setting it to 0 for if break to go back to the choosing your attribute loop
+                                        if (yOrN == 'y' || yOrN == 'Y') {
+                                            hpOrStr = 0;
+                                            break;
+                                        } else if (yOrN == 'n' || yOrN == 'N') {
+                                            h1.attPoints += pointAllocate;
+                                            h1.STR -= pointAllocate;
+                                            break;
+                                        }
+
+                                    }
+                                }
+                                if (h1.attPoints == 0) {
+                                    System.out.println("Points Left: " + h1.attPoints);
+                                    System.out.println("HP: " + h1.HP);
+                                    System.out.println("STR: " + h1.STR);
+                                    characterCreated = true;
+                                    break;
+                                }
+                            }
+                            if (hpOrStr == 0) {
+                                break;
                             }
 
-                        }
+                        } while (h1.attPoints != 0);
                     }
                 }
+                System.out.println("You're ready to start!");
+                break;
             }
-        }
+        } while (!characterCreated);
     }
 
     public static void main(String[] args) {
